37dde921d3b882a9795aa7c9fab49a33278f7568
mako542b <marcin.piotr.konopka@gmail.com>
2022-11-21 19:21:00 +0100

    commit

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'paper-scissors-rock/',
+  base: 'https://mako542b.github.io/paper-scissors-rock/',
   plugins: [react()]
 })
